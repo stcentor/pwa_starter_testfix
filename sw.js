f0309a7f80d836515ab7c074fa6eca1ce13523b9
stcentor <skyler.tangneycentor@gmail.com>
2019-10-09 15:28:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,19 +9,26 @@ var filesToCache = [
   'views/tones.ejs',
   'views/comments.ejs',
   'css/style.css',
-  'main.js'
- ];
- 
+  'js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(cacheName).then(function(cache){
-    return cache.addAll(filesToCache);
+    console.log("installing");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
-    );
-  });
-  
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(response){
-  return response || fetch(e.request);
-  })
   );
- });
+});
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
