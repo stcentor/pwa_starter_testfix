8ad8aaa2b321ee67178d16bd8df0f0459b3cfede
stcentor <skyler.tangneycentor@gmail.com>
2019-10-09 15:08:28 -0500

    created sw

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/tones',
+  '/comments',
+  'app.js',
+  'manifest.json',
+  'views/index.ejs',
+  'views/tones.ejs',
+  'views/comments.ejs',
+  'css/style.css',
+  'main.js'
+ ];
+ 
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(cacheName).then(function(cache){
+    return cache.addAll(filesToCache);
+    })
+    );
+  });
+  
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response){
+  return response || fetch(e.request);
+  })
+  );
+ });
